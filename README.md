9d19d604ede65223c31332073e66724f1b080c28
Afshin Karimi <afshin.karimi28@gmail.com>
2024-03-08 14:26:39 +0330

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Simple example Automation CI ArgoCD, k8s and spring
+
+This is demo for spring, k8s and spring project
+
+## Authors
+
+- [@AfshinKarimi](https://www.github.com/AfshinKarimi)
+
+
+
+
---
+++ src/main/java/com/snappbox/springargocdk8s/SpringArgocdK8sApplication.java
@@ -0,0 +1,13 @@
+package com.snappbox.springargocdk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArgocdK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringArgocdK8sApplication.class, args);
+	}
+
+}
